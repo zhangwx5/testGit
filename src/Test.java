1c2266398be70bf2847ed945a664c70421b49175
zhangwx5 <zhangwx5@asiainfo.com>
2017-03-21 13:58:27 +0800

    Git测试

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	/** 
+	 * <功能简述>
+	 * @author: zhangwx5 
+	 * @param args
+	 * @创建时间:  2017-3-21
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+	}
+
+}
