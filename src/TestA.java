79e7068f6c12e5c6d2f9f811cc5a34e13f645e23
zhangwx5 <zhangwx5@asiainfo.com>
2017-03-21 14:54:42 +0800

    TestA

---
+++ src/TestA.java
@@ -0,0 +1,17 @@
+
+public class TestA {
+
+	/** 
+	 * <功能简述>
+	 * @author: zhangwx5 
+	 * @param args
+	 * @创建时间:  2017-3-21
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		System.out.println("hello git!");
+		
+	}
+
+}
