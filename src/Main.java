121c098a9169dde46aad186efea792899800c087
zhangwx5 <zhangwx5@asiainfo.com>
2017-03-21 14:22:55 +0800

    main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	/** 
+	 * <功能简述>
+	 * @author: zhangwx5 
+	 * @param args
+	 * @创建时间:  2017-3-21
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
