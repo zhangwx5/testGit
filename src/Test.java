b53a8eb60220cdd1fe593701b1bdd0824d922b3b
zhangwx5 <zhangwx5@asiainfo.com>
2017-03-21 15:23:07 +0800

    aaa

--- src/Test.java
+++ src/Test.java
@@ -11,7 +11,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
 		System.out.println("hello git!");
-		
+		System.out.println("hello git!");
 	}
 
 }
